c14609bd7f457758f27391231cbe13c3d291d1d8
jay <jaypifiverr@gmail.com>
2022-06-05 13:28:18 -0400

    commit tree d68ba2e7a8471ba96245308532dc6ac577f99f0a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(name, greetings) {
+    let thanks = [];
+
+    for (let i = 0; i < name.length; i++) {
+      thanks.push(`Thank you, ${name[i]}, for the wonderful ${greetings} gift!`);
+      
+    }
+  
+    return thanks;
+
+}   
+  
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(number) {
+  let i = -1;
+  while (i < number) {
+    console.log(number--);
+  }
+
+}
+
+console.log(countDown(10))
